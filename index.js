b60a16d36f9c66f66114a1cc98408d03fc429b4b
kkgautam311 <45126741+kkgautam311@users.noreply.github.com>
2021-07-11 14:17:10 +0530

    Add files via upload
    Added functionality to play via keyboard instead of mouse click.

--- index.js
+++ index.js
@@ -1,54 +1,42 @@
+document.addEventListener("keypress",function (event){
+    switch (event["key"]) {
+        case "w":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
 
-let target = document.querySelectorAll(".drum");
+        case "a":
+            var kickBass = new Audio("sounds/kick-bass.mp3");
+            kickBass.play();
+            break;
 
-for (let i = 0 ; i<target.length ;i++){
+        case "s":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
 
-    target[i].addEventListener("click",function (){
+        case "d":
+            var crash = new Audio("sounds/tom-1.mp3");
+            crash.play();
+            break;
 
+        case "j":
+            var crash = new Audio("sounds/tom-2.mp3");
+            crash.play();
+            break;
 
-        this.classList.toggle("whitetext")
+        case "k":
+            var crash = new Audio("sounds/tom-3.mp3");
+            crash.play();
+            break;
 
-        switch (this.innerHTML) {
-            case "w":
-                var crash = new Audio("sounds/crash.mp3");
-                crash.play();
-                break;
+        case "l":
+            var crash = new Audio("sounds/tom-4.mp3");
+            crash.play();
+            break;
 
-            case "a":
-                var kickBass = new Audio("sounds/kick-bass.mp3");
-                kickBass.play();
-                break;
-
-            case "s":
-                var snare = new Audio("sounds/snare.mp3");
-                snare.play();
-                break;
-
-            case "d":
-                var crash = new Audio("sounds/tom-1.mp3");
-                crash.play();
-                break;
-
-            case "j":
-                var crash = new Audio("sounds/tom-2.mp3");
-                crash.play();
-                break;
-
-            case "k":
-                var crash = new Audio("sounds/tom-3.mp3");
-                crash.play();
-                break;
-
-            case "l":
-                var crash = new Audio("sounds/tom-4.mp3");
-                crash.play();
-                break;
-
-            default:
-                console.log(this.innerHTML)
-                break;
-        }
-    });
-}
-
-
+        default:
+            console.log(this.innerHTML)
+            break;
+    }
+});
